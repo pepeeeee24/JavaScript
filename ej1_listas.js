6a0a625f8856d33f470fa1b4b706bf30df2d12e6
pepeeeee24 <115158334+pepeeeee24@users.noreply.github.com>
2023-01-16 14:43:43 +0100

    Update ej1_listas.js

--- ej1_listas.js
+++ ej1_listas.js
@@ -1,11 +1,27 @@
     // Crea una lista de números pares del 2 al 20 utilizando un bucle while.
-
+var lista_pares=[];
+var i = 0;
+while(i<=20){
+if(i%2==0){
+    lista_pares.push(i);
+}
+i = i+ 1;
+}
+ console.log(lista_pares);
     // Crea una lista de números impares del 1 al 19 utilizando un bucle for.
-    
+    var lista_impares=[];
+
+for( var index = 1 ; index<=19; index++){
+    if (index % 2 == 1){
+        lista_impares.push(index);
+    }
+}
+console.log(lista_impares);
     // Crea una lista de comida.
-    
+    var lista_comida=["tortilla, jamon,puchero,croquetas,paella"];
+    console.log(lista_comida);
     // Agrega un elemento al principio de la lista de comida que creaste en el ejercicio anterior.
-    
+     lista_comida.unshift("sandia")
     // Accede al segundo elemento de la lista de comida y cambia su valor por otro diferente.
     
     // Recorre la lista de comida utilizando un bucle while y muestra cada elemento en la consola.
